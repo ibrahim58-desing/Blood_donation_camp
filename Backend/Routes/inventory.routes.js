802190aa842a45bc4e224edf62733ee513179603
ibrahim58-desing <ciribu58@gmail.com>
2026-01-26 13:19:23 +0530

    updated inventory routes

---
+++ Backend/Routes/inventory.routes.js
@@ -0,0 +1,82 @@
+import express from 'express'
+import { inventoryValidationSchema } from '../Utils/InventoryValidationSchema.js'
+import { Inventory } from '../Mongoose/Model/InventorySchema.js'
+import { checkSchema, validationResult, matchData, matchedData } from 'express-validator'
+
+const router = express.Router()
+
+router.get('/inventory', async (req, res) => {
+    try {
+        const units = await Inventory.find("donor_id", "donor_code name");
+        res.json(units)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+router.get('/inventory/:id', async (req, res) => {
+
+    try {
+        const units = await Inventory.findById(req.parms.id)
+        if (!units)
+            res.status(404).json({ error: "Inventory not found" })
+        res.json(units)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
+})
+
+router.post('/inventory',
+    checkSchema(inventoryValidationSchema),
+    async (req, res) => {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+        const data = matchData(req)
+
+        try {
+            const newunit = new Inventory(data)
+            await newunit.save()
+            res.status(201).json(newunit);
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+
+    })
+
+router.post('/inventory/:id',
+    checkSchema(inventoryValidationSchema),
+    async (req, res) => {
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+        const data = matchedData(req)
+
+        try {
+            const unit = await Inventory.findByIdAndUpdate(req.parms.id, data, { new: true })
+
+            if (!unit) return res.status(404).json({ error: "Inventory not found" });
+
+            res.json(unit);
+
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    }
+)   
+
+router.delete("/inventory/:id", async (req, res) => {
+  try {
+    const deleted = await Inventory.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: "Unit not found" });
+    }
+    res.json({ message: "Unit removed successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
