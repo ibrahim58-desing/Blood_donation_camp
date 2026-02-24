d88dcdd43c6b8f8f3e4c62b3bfa3bcabbae26175
ibrahim58-desing <ciribu58@gmail.com>
2026-02-24 13:01:16 +0530

    updted api url

--- blood-bank-frontend/src/components/api.js
+++ blood-bank-frontend/src/components/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://blood-donation-camp-backends.onrender.com';
+const API_URL = 'https://blood-donation-camp-backends.onrender.com';
 
-export default API_BASE_URL;
+export default API_URL;
